9f9e44d8eccfbe15c337fb1da06e690692831876
mushugit <marmouchon@gmail.com>
2015-09-14 23:40:50 +0200

    Moved generic instantiated methods to DataAnalyzer.cs

--- OpenTTDTool/DataAnalyzers/PseudoSpriteAnalyzer.cs
+++ OpenTTDTool/DataAnalyzers/PseudoSpriteAnalyzer.cs
@@ -14,12 +14,6 @@ namespace OpenTTDTool.DataAnalyzers
     public abstract class PseudoSpriteAnalyzer : DataAnalyzer
     {
 
-        /// <summary>
-        /// Reading properties moved the readindex, so if the property has already been read, it doesn't move
-        /// </summary>
-        protected Dictionary<string, string> AlreadyReadProperties { get; private set; }
-        protected int ReadIndex { get; set; }
-
         protected bool Cleaned;
 
         public PseudoSpriteAnalyzer(List<string> parsedText, int rowNumber, int initialReadIndex = Constants.INDEX_CLEANABLE)
@@ -36,8 +30,6 @@ namespace OpenTTDTool.DataAnalyzers
 
         public abstract void CleanParsedText();
 
-        #region Row individual value processing
-        #region Functional analyzers
         public static Actions? ReadAction(List<string> parsedText)
         {
             var hexValue = IntHelper.ConvertFromHex(parsedText[Constants.INDEX_ACTIONS]);
@@ -54,129 +46,5 @@ namespace OpenTTDTool.DataAnalyzers
         {
             return ReadIntFieldFromHex(FieldSizes.ExtendedByte, nameof(ReadCode)).Value;
         }
-        #endregion
-        #region Technical analyzers
-        protected T? ReadEnum<T>(int index) where T : struct
-        {
-            if (!typeof(T).IsEnum)
-                throw new ArgumentException();
-
-            var hexValue = ReadHexData(index);
-            return Enum.IsDefined(typeof(T), hexValue) ? (T)(object)hexValue : (T?)null;
-        }
-
-        protected int ReadHexData(int index)
-        {
-            return IntHelper.ConvertFromHex(ParsedText[index]);
-        }
-
-        protected bool TryReadHexData(int index, out int result)
-        {
-            return IntHelper.TryConvertFromHex(ParsedText[index], out result);
-        }
-
-        protected string ReadStringField(FieldSizes fieldSize, string fieldName = null)
-        {
-            return ReadRawValue(fieldSize, fieldName);
-        }
-
-        protected int? ReadIntLitteralField(FieldSizes fieldSize, string fieldName = null)
-        {
-            string raw = null;
-
-            try
-            {
-                raw = ReadRawValue(fieldSize, fieldName);
-
-                int intValue;
-                if (int.TryParse(raw, out intValue))
-                    return intValue;
-                else
-                    return null;
-            }
-            catch
-            {
-                log.Error($"Row {RowNumber}: Error parsing litteral int value \"{raw}\" on ReadIndex {ReadIndex}");
-                throw;
-            }
-        }
-
-        protected int? ReadIntFieldFromHex(FieldSizes fieldSize, string fieldName = null)
-        {
-            string raw = null;
-            try
-            {
-                raw = ReadRawValue(fieldSize, fieldName);
-
-                int intValue;
-                if (IntHelper.TryConvertFromHex(raw, out intValue))
-                    return intValue;
-                else
-                    return null;
-            }
-            catch
-            {
-                log.Error($"Row {RowNumber}: Error parsing int value \"{raw}\" on ReadIndex {ReadIndex}");
-                throw;
-            }
-        }
-
-        private string ReadRawValue(FieldSizes fieldSize, string fieldName = null)
-        {
-            var textValueBuilder = new StringBuilder();
-
-            if (!String.IsNullOrWhiteSpace(fieldName) && AlreadyReadProperties.ContainsKey(fieldName))
-                return AlreadyReadProperties[fieldName];
-
-            var length = Constants.FieldLengths[fieldSize];
-
-            if (fieldSize == FieldSizes.ExtendedByte)
-            {
-                var firstByte = ReadIntFieldFromHex(FieldSizes.Byte);
-                if (firstByte == 0xFF)
-                {
-                    length = 2;
-                }
-                else
-                {
-                    //Value already read
-                    textValueBuilder = new StringBuilder(firstByte.ToString());
-                    length = null;
-                }
-            }
-
-            if (length.HasValue)
-            {
-                for (var i = (ReadIndex - 1) + length.Value; i >= ReadIndex; i--)
-                {
-                    if (i >= ParsedText.Count)
-                    {
-                        log.Error($"Row {RowNumber}: index would be out of range (index: {i}, Parsed text length: {ParsedText.Count}).");
-                        return null;
-                    }
-                    textValueBuilder = textValueBuilder.Append(ParsedText[i]);
-                }
-
-            }
-            else
-            {
-                if (fieldSize == FieldSizes.MultipleBytes)
-                {
-                    //TODO : pas traité pour le moment. List of always refittable cargo types / never refittable cargo types
-                    var nbData = ReadIntFieldFromHex(FieldSizes.Byte);
-                    length = nbData;
-                }
-            }
-
-            if (length.HasValue)
-                ReadIndex += length.Value;
-
-            var textValue = textValueBuilder.ToString();
-            if (!String.IsNullOrWhiteSpace(fieldName))
-                AlreadyReadProperties.Add(fieldName, textValue);
-            return textValue;
-        }
-        #endregion
-        #endregion
     }
 }
